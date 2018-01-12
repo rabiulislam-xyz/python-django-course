00ce63c6831a9f60483020c207ee654c6df7f160
Rabiul Islam <32502709+rabiulislam993@users.noreply.github.com>
2018-01-12 22:24:28 +0600

    Update logical_oparetors.py

--- python/class-2/logical_oparetors.py
+++ python/class-2/logical_oparetors.py
@@ -1,1 +1,25 @@
+>>> True and True
+True
+>>> True and False
+False
+>>> True or False
+True
+>>> False or False
+False
+>>> not False
+True
+>>> not True
+False
 
+>>> 2 == 2 and 3 == 3
+True
+>>> 2 == 2 and 3 == 4
+False
+>>> 2 == 2 or 3 == 4
+True
+>>> 2 == 5 or 3 == 4
+False
+>>> 2 == 5
+False
+>>> not 2 == 5
+True
