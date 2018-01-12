a5cda15caeb6436b69c1e662943ebd44a4b055c4
Rabiul Islam <32502709+rabiulislam993@users.noreply.github.com>
2018-01-12 22:22:19 +0600

    Update type_casting.py

--- python/class-2/type_casting.py
+++ python/class-2/type_casting.py
@@ -1,1 +1,32 @@
+a = 1
+type(a)
 
+a = bool(a)
+type(a)
+
+a = float(a)
+type(a)
+
+a = str(a)
+type(a)
+
+b = "Hello"
+type(b)
+
+b = bool(b)
+type(b)
+
+b = int(b)
+type(b)
+
+b = float(b)
+type(b)
+
+c = "123"
+type(c)
+
+c = int(c)
+type(c)
+
+c = float(c)
+type(c)
