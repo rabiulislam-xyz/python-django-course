dbcb84b65295e0b38a9843bf109e8178f0db8121
Rabiul Islam <32502709+rabiulislam993@users.noreply.github.com>
2018-01-12 22:54:03 +0600

    Update formatting.py

--- python/class-2/formatting.py
+++ python/class-2/formatting.py
@@ -1,1 +1,19 @@
+text = "My favorite programming language is " + "python"
+print(text)
 
+text = "My favorite programming language is {}".format("python")
+print(text)
+
+language1 = 'python'
+language2 = 'javascript'
+
+text = "My favorite programming language is " + language1 + " and " + language2
+print(text)
+
+text = "My favorite programming language is {} and {}".format(language1, language2)
+
+name = "Habib"
+age = 13
+
+text = "{} is {} years old".format(name, age)
+print(text)
