0ccd4ba0cb4a535588abfb3bf9d6ff080e6c7967
Rabiul Islam <32502709+rabiulislam993@users.noreply.github.com>
2018-01-12 21:21:20 +0600

    Update variable_naming_convention.py

--- python/class-2/variable_naming_convention.py
+++ python/class-2/variable_naming_convention.py
@@ -1,1 +1,28 @@
+'''
+ভ্যারিয়েবলের নাম একটি মাত্র শব্দে হতে হবে
+ভ্যারিয়েবলের নামে শুধুমাত্র লেটার, নাম্বার অথবা আন্ডারস্কোর (_) থাকতে পারবে
+ভ্যারিয়েবলের নাম কখনোই নাম্বার দ্বারা শুরু করা যাবেনা (কিন্তু আন্ডারস্কোর দ্বারা শুরু করা যাবে)
+পাইথনের রিজার্ভ কিওয়ার্ড গুলো ভ্যারিয়েবলের নাম হিসেবে রাখা যাবেনা
+'''
 
+variable = "valid name"
+variableName = "valid name"
+variable_name = "valid name"
+variable123 = "valid name"
+_variable = "valid name"
+VARIABLE = "valid name"
+__variable123NAME__ = "valid name"
+
+
+variable-name = "NOT VALID"
+variable name = "NOT VALID"
+"variable" = "NOT VALID"
+123variable = "NOT VALID"
+vari@ble = "NOT VALID"
+finally = "NOT VALID"
+
+
+# variable name are case sensitive
+name = 'variable 1'
+Name = 'variable 2'
+NAME = 'variable 3'
