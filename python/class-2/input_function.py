b4b664d1131517e8cf803f14bc24dfd3ff9d3a24
Rabiul Islam <32502709+rabiulislam993@users.noreply.github.com>
2018-01-12 22:17:44 +0600

    Update input_function.py

--- python/class-2/input_function.py
+++ python/class-2/input_function.py
@@ -1,1 +1,15 @@
+print("What's Your name?")
+name = input()
+print(a)
+print(type(a))
 
+print("give me a number please!")
+number1 = input()
+print(number1)
+print(type(number1))
+
+number2 = input("give me another number!")
+print(number2)
+print(type(number2))
+
+print(number1 + number2)
