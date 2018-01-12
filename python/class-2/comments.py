b8d240b20a9d69f20ee288f187fb374330c19e80
Rabiul Islam <32502709+rabiulislam993@users.noreply.github.com>
2018-01-12 22:39:51 +0600

    Update comments.py

--- python/class-2/comments.py
+++ python/class-2/comments.py
@@ -1,1 +1,16 @@
+a = 1 # this is single line comment 
+# and also this
 
+# multiline comments is
+# combination of single line
+# comments
+
+print("everything is ignored after # hash sign")
+
+"""
+this is a
+doc string
+used for 
+multiline 
+comments
+"""
